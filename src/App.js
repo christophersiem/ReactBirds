eb1f2cc672aba8cbca233a73e08ffb46e896315c
“Christopher” <cman1609@gmail.com>
2020-06-17 17:12:16 +0200

    Add FetchState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SiteHeader from "./components/SiteHeader";
 import SiteContent from "./components/SiteContent";
 import SiteFooter from "./components/SiteFooter";
 import "./App.css"
-
-
-const birdsArray = [
-    {
-        title: "Big Bird",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto debitis excepturi iure iusto maiores officia ratione. Dolorem esse est itaque?",
-        imageSource: "https://cdn.prod.www.spiegel.de/images/188c5f65-534b-41b3-be01-2650baa51794_w948_r1.77_fpx50_fpy48.jpg",
-
-    },
-    {
-        title: "Small Bird",
-        text: "Loading...",
-        imageSource: "https://www.nabu.de/imperia/md/nabu/images/arten/tiere/voegel/lerchen/160118-nabu-feldlerche-gaby-schroeder.jpeg",
-
-    },
-    {
-        title: "Birdy",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto debitis excepturi iure iusto maiores officia ratione. Dolorem esse est itaque?",
-        imageSource: "https://amp.volksstimme.de/storyimage/MA/20190515/DPA/305159999/AR/0/AR-305159999.jpg&MaxW=1200&Imageversion=default",
-    },
-
-];
+import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 
 async function getAllBirds() {
     const response = await fetch("https://api.jsonbin.io/b/5ee9e76c0e966a7aa36b3fd4");
@@ -33,53 +12,61 @@ async function getAllBirds() {
 
 }
 
-
 function App() {
 
-    const [content, setContent] = React.useState(birdsArray[1]);
+    const [content, setContent] = useState();
+    const [birds, setBirds] = useState([]);
+    const[fetchState, setFetchState] = useState();
+
+
+    useEffect(() => {
+        setFetchState("LOADING");
+        getAllBirds().then(data => {
+            setContent(data[0]);
+            setBirds(data);
+            setFetchState("SUCCESS");
+        }).catch(()=> setFetchState("FAILED"));
+    }, []);
 
 
     function setBigBirdAsContent() {
-        getAllBirds().then((response) => setContent(response[0]));
+        setContent(birds[0]);
     }
 
     function setSmallBirdAsContent() {
-        getAllBirds().then((response) => setContent(response[1]));
-
+        setContent(birds[1]);
     }
 
     function setBirdyAsContent() {
-        getAllBirds().then((response) => setContent(response[2]));
+        setContent(birds[2]);
     }
 
-    function setJürgenAsContent() {
-        getAllBirds().then((response) => setContent(response[3]));
+    function setJuergenAsContent() {
+        setContent(birds[3]);
     }
-
 
     function setRandomAsContent() {
 
-        getAllBirds().then(birds => {
-            const index = Math.floor(Math.random() * birds.length);
-            setContent(birds[index]);
-        })
+        const index = Math.floor(Math.random() * birds.length);
+        setContent(birds[index]);
 
     }
-
 
     return (
 
         <div className="app">
+
             <SiteHeader/>
+            {fetchState ==="LOADING" && <LoadingSpinner/>}
             <div className="button">
                 <button onClick={setSmallBirdAsContent}>Small bird</button>
                 <button onClick={setBigBirdAsContent}>Big bird</button>
                 <button onClick={setBirdyAsContent}>Birdy</button>
-                <button onClick={setJürgenAsContent}>Jürgen</button>
+                <button onClick={setJuergenAsContent}>Jürgen</button>
                 <button onClick={setRandomAsContent}>Random bird</button>
 
             </div>
-            <SiteContent cardContent={content}/>
+            {content && <SiteContent cardContent={content}/>}
             <SiteFooter/>
 
         </div>
---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./LoadingSpinner.css"
+
+export default function LoadingSpinner() {
+
+return(
+
+    <div className="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+)
+}
